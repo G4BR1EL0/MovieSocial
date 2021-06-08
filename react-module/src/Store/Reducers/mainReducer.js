fb3c988bedff10046676902c2210418922a24fde
G4BR1EL0 <gabrielabarca.91@gmail.com>
2021-06-08 03:15:45 +0200

    remove criteria fron store, change header background color

--- react-module/src/Store/Reducers/mainReducer.js
+++ react-module/src/Store/Reducers/mainReducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 import loginReducer from './loginReducer';
 import movieReducer from './movieReducer';
 import userReducer from './userReducer';
-import searchReducer from './searchReducer';
 import valorationReducer from './valorationReducer';
 
 
@@ -10,7 +9,6 @@ const mainReducer = combineReducers({
     token: loginReducer,
     user: userReducer,
     movie: movieReducer,
-    criteria: searchReducer,
     valoration: valorationReducer
 }) 
 
